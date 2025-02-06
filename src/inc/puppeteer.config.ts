7ad0662dbea975bf269647f67b18b114dd10ada7
obrian-code <lgjhons@gmail.com>
2025-02-06 12:36:01 -0500

    fix: eliminar exportacion de config,proxy,userAgen

--- src/inc/puppeteer.config.ts
+++ src/inc/puppeteer.config.ts
@@ -5,13 +5,11 @@ import { ConfigService } from '@nestjs/config';
 
 puppeteer.use(AdblockerPlugin());
 
-export default puppeteer;
-
 const config = new ConfigService();
 
-export const proxy = `http://scraperapi:${config.get<string>('SCRAPERAPI_KEY')}@proxy-server.scraperapi.com:8001?country=PE`;
+const proxy = `http://scraperapi:${config.get<string>('SCRAPERAPI_KEY')}@proxy-server.scraperapi.com:8001?country=PE`;
 
-export const userAgent =
+const userAgent =
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36';
 
 export const launchBrowser = async () => {
--- src/sunarp/sunarp.service.ts
+++ src/sunarp/sunarp.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
-import puppeteer, { userAgent } from 'src/inc/puppeteer.config';
+import { launchBrowser } from 'src/inc/puppeteer.config';
 import Tesseract from 'tesseract.js';
 
 @Injectable()
@@ -10,75 +10,80 @@ export class SunarpService {
   constructor(private configService: ConfigService) {}
 
   async vehiculo_placa(placa: string) {
-    const browser = await puppeteer.launch({
-      headless: false,
-      args: ['--no-sandbox'],
-    });
-    /*     --proxy-server=${proxy} */
+    let browser, page;
+    try {
+      ({ browser, page } = await launchBrowser());
+      await page.goto(
+        `${this.configService.get<string>('SUNARP_VECHICULO_PLACA')}`,
+        {
+          waitUntil: 'networkidle2',
+        },
+      );
 
-    const page = await browser.newPage();
-    await page.setUserAgent(userAgent);
-    await page.goto(this.configService.get<string>('SUNARP_VECHICULO_PLACA'), {
-      waitUntil: 'networkidle2',
-    });
+      await page.waitForSelector('#nroPlaca', { timeout: 10000 });
 
-    await page.waitForSelector('#nroPlaca', { timeout: 10000 });
+      // Ingresar el número de placa 'PC3060'
+      await page.evaluate((__placa: string) => {
+        const input = document.getElementById('nroPlaca') as HTMLInputElement;
+        if (input) {
+          input.value = __placa;
+          input.dispatchEvent(new Event('input', { bubbles: true }));
+        }
+      }, placa);
 
-    // Ingresar el número de placa 'PC3060'
-    await page.evaluate((__placa: string) => {
-      const input = document.getElementById('nroPlaca') as HTMLInputElement;
-      if (input) {
-        input.value = __placa;
-        input.dispatchEvent(new Event('input', { bubbles: true }));
+      // Obtener el contenido HTML de la página
+      const content = await page.content();
+      const $ = cheerio.load(content);
+
+      // Obtener la URL de la imagen en Base64
+      const base64Image = $('img#image').attr('src');
+      console.log({ base64Image });
+      if (!base64Image) {
+        console.error('No se encontró la imagen.');
+        await browser.close(); // Asegúrate de cerrar el navegador en caso de error
+        return null; // Devolver null si no se encontró la imagen
       }
-    }, placa);
 
-    // Obtener el contenido HTML de la página
-    const content = await page.content();
-    const $ = cheerio.load(content);
+      // Extraer texto del captcha
+      const imageResponse = await axios.get(base64Image, {
+        responseType: 'arraybuffer',
+      });
+      const imageBuffer = Buffer.from(imageResponse.data, 'binary');
 
-    // Obtener la URL de la imagen en Base64
-    const base64Image = $('img#image').attr('src');
-    console.log({ base64Image });
-    if (!base64Image) {
-      console.error('No se encontró la imagen.');
-      await browser.close(); // Asegúrate de cerrar el navegador en caso de error
-      return null; // Devolver null si no se encontró la imagen
+      const {
+        data: { text },
+      } = await Tesseract.recognize(imageBuffer, 'eng', {
+        logger: (m) => console.log(m), // Progreso de la OCR
+      });
+      console.log({ text });
+      console.log('Texto extraído de la imagen:', text.trim());
+
+      // Ingresar el texto del captcha
+      await page.type('#codigoCaptcha', text.trim(), { delay: 100 });
+
+      // Hacer clic en el botón de login
+      await page.click(
+        '.ant-btn.btn-sunarp-green.ant-btn-primary.ant-btn-lg.btn',
+      );
+
+      const base64ImageV = await page.$eval(
+        '.container-data-vehiculo img',
+        (img) => img.src,
+      );
+
+      if (!base64Image) {
+        console.error('No se encontró la imagen.');
+        return;
+      }
+      await browser.close(); // Cerrar el navegador
+
+      return base64ImageV; // Devolver la imagen en Base64
+    } catch (error) {
+      return { error: 'Error al procesar el SUNAT.' };
+    } finally {
+      if (browser) {
+        await browser.close(); // Asegúrate de cerrar el navegador
+      }
     }
-
-    // Extraer texto del captcha
-    const imageResponse = await axios.get(base64Image, {
-      responseType: 'arraybuffer',
-    });
-    const imageBuffer = Buffer.from(imageResponse.data, 'binary');
-
-    const {
-      data: { text },
-    } = await Tesseract.recognize(imageBuffer, 'eng', {
-      logger: (m) => console.log(m), // Progreso de la OCR
-    });
-    console.log({ text });
-    console.log('Texto extraído de la imagen:', text.trim());
-
-    // Ingresar el texto del captcha
-    await page.type('#codigoCaptcha', text.trim(), { delay: 100 });
-
-    // Hacer clic en el botón de login
-    await page.click(
-      '.ant-btn.btn-sunarp-green.ant-btn-primary.ant-btn-lg.btn',
-    );
-
-    const base64ImageV = await page.$eval(
-      '.container-data-vehiculo img',
-      (img) => img.src,
-    );
-
-    if (!base64Image) {
-      console.error('No se encontró la imagen.');
-      return;
-    }
-    await browser.close(); // Cerrar el navegador
-
-    return base64ImageV; // Devolver la imagen en Base64
   }
 }
